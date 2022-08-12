f3131402580d777d7f6a2f719ce8cd12349e53d4
black <gikigiio@gmail.com>
2022-08-12 17:29:28 -0300

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    test_product = Product(
+        1,
+        "produto",
+        "empresa",
+        "01/01/2020",
+        "01/01/2021",
+        "12345",
+        "armazenar em geladeira",
+    )
+
+    assert (
+        test_product.__repr__() == f"O produto {test_product.nome_do_produto}"
+        f" fabricado em {test_product.data_de_fabricacao}"
+        f" por {test_product.nome_da_empresa}"
+        f" com validade até {test_product.data_de_validade}"
+        f" precisa ser armazenado {test_product.instrucoes_de_armazenamento}."
+    )
