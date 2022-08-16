f61a9d07311022d23074be3b29966d3e5864c44c
black <gikigiio@gmail.com>
2022-08-16 17:09:28 -0300

    Requisito 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from ..reports.complete_report import CompleteReport
 
@@ -8,8 +9,13 @@ from ..reports.simple_report import SimpleReport
 class Inventory:
     def import_data(path, type):
         with open(path, "r") as file:
-            reader = csv.DictReader(file)
-            data = list(reader)
+            if path.endswith(".csv"):
+                reader = csv.DictReader(file)
+                data = list(reader)
+
+            if path.endswith(".json"):
+                data = json.load(file)
+
             if type == "simples":
                 return SimpleReport.generate(data)
             return CompleteReport.generate(data)
